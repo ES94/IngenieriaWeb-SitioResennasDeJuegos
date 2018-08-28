6fa520a2e7b30366f03b726cc546599174de5553
ES94 <e.stradella94@outlook.com>
2018-08-27 21:25:28 -0300

    Commit Nº 2 - 27/08/2018

---
+++ JuegoSennas_SitoWeb/JuegoSennas_SitoWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JuegoSennas_SitoWeb.Startup))]
+namespace JuegoSennas_SitoWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
