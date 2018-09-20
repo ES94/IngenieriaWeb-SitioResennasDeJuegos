54c4e38f4d3313d47e8d6373c67bbb7558f350e6
ES94 <e.stradella94@outlook.com>
2018-09-20 05:40:03 -0300

    Commit Nº 8 - 20/09/2018

---
+++ JuegoSennas_SitoWeb/JuegoSennas_SitoWeb/Controllers/ContenidoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using JuegoSennas_SitoWeb.Models;
+using System.Data.SqlClient;
+
+namespace JuegoSennas_SitoWeb.Controllers
+{
+    public class ContenidoController : Controller
+    {
+        public ContenidoController()
+        {
+        }
+
+        public ActionResult Reseñas()
+        {
+            return View();
+        }
+
+        public ActionResult Reseñas(int idCategoria)
+        {
+            return View();  // Aplicar filtro según categoría de juegos elegida.
+        }
+
+        [Authorize]
+        public ActionResult CrearReseña()
+        {
+            return View();
+        }
+    }
+}
